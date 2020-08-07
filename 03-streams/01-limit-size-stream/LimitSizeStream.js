2b2476b77cb37064c6fa83a9337f096909737fee
Andrew Iashchuk <iashchuk.andrii@pdffiller.team>
2020-08-07 14:48:32 +0300

    Homework 3 was completed

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit || 8;
+    this.transmittedDataAmount = 0;
+    this.encoding = options.encoding;
   }
 
   _transform(chunk, encoding, callback) {
+    this.transmittedDataAmount += chunk.toString(this.encoding).length;
 
+    if (this.transmittedDataAmount > this.limit) {
+      callback(new LimitExceededError('Was reached data limit'));
+      return;
+    }
+    callback(null, chunk, encoding);
   }
 }
 
--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,34 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.encoding = options.encoding;
+    this.temp = '';
+  }
+
+  getTemp(data) {
+    if (this.temp.length) {
+      data = this.temp + data;
+      this.temp = '';
+    }
+    const index = data.indexOf(os.EOL);
+
+    if (index === -1) return data;
+
+    this.push(this.temp + data.slice(0, index));
+    return this.getTemp(data.slice(index + 1));
   }
 
   _transform(chunk, encoding, callback) {
+    const data = chunk.toString(this.encoding);
+    this.temp = this.getTemp(data);
+    callback();
   }
 
   _flush(callback) {
+    if (this.temp.length) {
+      this.push(this.temp);
+    }
+    callback();
   }
 }
 
