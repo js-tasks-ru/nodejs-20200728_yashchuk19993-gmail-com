c798ad3a6f221c8bb4ecfec16d00a8dc2c5a355a
Andrew Iashchuk <iashchuk.andrii@pdffiller.team>
2020-08-11 17:51:56 +0300

    was change piplen to pipe

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,6 +1,5 @@
 const url = require('url');
 const http = require('http');
-const {pipeline} = require('stream');
 const path = require('path');
 const fs = require('fs');
 const LimitSizeStream = require('./LimitSizeStream');
@@ -23,32 +22,40 @@ server.on('request', (req, res) => {
         const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
         const limitSizeStream = new LimitSizeStream({limit: ONE_MEGA_BYTE});
 
-        pipeline(
-            req,
-            limitSizeStream,
-            writeStream,
-            (error) => {
-              if (error) {
-                const {code} = error;
-                if (compareCode(code, 'EEXIST')) {
-                  res.statusCode = 409;
-                  res.end('File exists');
-                } else if (compareCode(code, 'LIMIT_EXCEEDED')) {
-                  res.statusCode = 413;
-                  res.end('File is more that limit');
-                  debugger;
-                } else if (compareCode(code, '‌ERR_STREAM_PREMATURE_CLOSE')) {
-                  fs.unlink(filepath, () =>{});
-                } else {
-                  res.statusCode = 500;
-                  res.end('Internal server error');
-                }
-              } else {
-                res.statusCode = 201;
-                res.end('File was received');
-              }
-            },
-        );
+        req.on('close', function() {
+          if (this.aborted) {
+            fs.unlink(filepath, () =>{});
+          }
+        });
+
+
+        writeStream.on('error', (error) => {
+          if (compareCode(error.code, 'EEXIST')) {
+            res.statusCode = 409;
+            res.end('File exists');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal server error');
+          }
+        });
+
+        limitSizeStream.on('error', (error) => {
+          if (compareCode(error.code, 'LIMIT_EXCEEDED')) {
+            res.statusCode = 413;
+            res.end('File is more that limit');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal server error');
+          }
+          fs.unlink(filepath, () =>{});
+        });
+
+        writeStream.on('finish', () => {
+          res.statusCode = 201;
+          res.end('File was received');
+        });
+
+        req.pipe(limitSizeStream).pipe(writeStream);
       }
       break;
     default:
