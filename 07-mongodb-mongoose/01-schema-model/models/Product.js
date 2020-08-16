a2d52f1fd2c4f78b99006e75517b7b843c0ff0bb
Andrew Iashchuk <iashchuk.andrii@pdffiller.team>
2020-08-16 15:53:29 +0300

    tast 07-01 product and category models were added

--- 07-mongodb-mongoose/01-schema-model/models/Product.js
+++ 07-mongodb-mongoose/01-schema-model/models/Product.js
@@ -2,6 +2,28 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+  subcategory: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
