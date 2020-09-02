caf4e6ffce952d71b75c3b47267c98d3abcae71e
Andrew Iashchuk <iashchuk.andrii@pdffiller.team>
2020-09-02 22:00:17 +0300

    hw 8 02-aouth was added

--- 08-authentication/02-oauth/config.js
+++ 08-authentication/02-oauth/config.js
@@ -1,8 +1,10 @@
+const {password, dbName} = require('../../config');
+
 module.exports = {
   mongodb: {
     uri: (process.env.NODE_ENV === 'test') ?
-      'mongodb://localhost/7-module-2-task' :
-      'mongodb://localhost/any-shop',
+      'mongodb://localhost/6-module-2-task' :
+      `mongodb+srv://db_user:${password}@cluster0.lrf6s.mongodb.net/${dbName}?retryWrites=true&w=majority`,
   },
   crypto: {
     iterations: (process.env.NODE_ENV === 'test' ? 1 : 12000),
